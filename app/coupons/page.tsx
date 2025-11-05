7337c19a8b85bb82fdff447675b42a8bc01be5f5
jesusecaceres <jesusecaceres@gmail.com>
2025-11-04 21:05:12 -0800

    Create page.tsx

---
+++ app/coupons/page.tsx
@@ -0,0 +1,8 @@
+export default function Coupons() {
+  return (
+    <main style={{ maxWidth: 1200, margin: "24px auto", padding: "0 20px" }}>
+      <h1>Cupones</h1>
+      <p>Ofertas locales para nuestra comunidad trabajadora.</p>
+    </main>
+  );
+}
